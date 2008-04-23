1a9bef64409eda7d3586b66a0da16f84af1598b1
afdm76 <afdm76@e099a375-04f9-0310-9d5f-a741eaff62e1>
2008-04-23 16:47:37 +0000

    RF postmortem: work on the frontend and file selection if given an argument frontend will hand over to file selectionfunction. TODO remove hard coded paths in selection and maybe make option list
    git-svn-id: svn+ssh://serv0002.cs.diamond.ac.uk/home/subversion/repos/controls/diamond/trunk/ioc/CS/CS-DI-IOC-07@18789 e099a375-04f9-0310-9d5f-a741eaff62e1

--- rf_pm_file_select.py
+++ rf_pm_file_select.py
@@ -2,7 +2,6 @@
 
 # retrieve the file information from a selected folder
 # sort the files by last modified date/time and display in order newest file first
-# tested with Python24    vegaseat    21jan2006
 import os
 import glob
 import time
@@ -10,47 +9,42 @@ import time
 
 def file_list(root):
     #root is the path to the files.
-    #root = '/home/ops/rf/RF-libera/RF_Postmortems/' # one specific folder
     date_file_list = []
     for folder in glob.glob(root):
 #        print "folder =", folder
      # select the type of file, for instance *.mat or all files *.*
         for file in glob.glob(folder + '/rf_postmortem-*.mat'):
-            # retrieves the stats for the current file as a tuple
-            # (mode, ino, dev, nlink, uid, gid, size, atime, mtime, ctime)
-            # the tuple element mtime at index 8 is the last-modified-  date
-            stats = os.stat(file)
-            # create tuple (year yyyy, month(1-12), day(1-31), hour(0-23), minute(0-59), second(0-59),
-            # weekday(0-6, 0 is monday), Julian day(1-366), daylight flag(-1,0 or 1)) from seconds since epoch
-            # note:  this tuple can be sorted properly by date and time
-            lastmod_date = time.localtime(stats[8])
+            # get date form filename
+            pm_date = file[55:-4]
+            pm_date = time.strptime(pm_date,"%Y-%m-%dT%H:%M:%S")
             # create list of tuples ready for sorting by date
-            date_file_tuple = lastmod_date, file
+            date_file_tuple = (pm_date, file)
             date_file_list.append(date_file_tuple)
 
     date_file_list.sort()
     date_file_list.reverse()  # newest mod date now first
-
+    # Changing from 1 list of 2D tupples to 2 tupples the length of the original list
+    timestamps = zip(*date_file_list)[0]
+    names = zip(*date_file_list)[1]
+    # converting from tupple to list to allow searching
+    names = list(names)
+    # getting the index of the required timestamp
+    ind = names.index("/home/ops/rf/RF-libera/RF_Postmortems/rf_postmortem-01-2008-04-17T10:17:22.mat")
+    required_ts = timestamps[ind]
     # only selecting the entries which match the latest timestamp.
-    selected_pms = [elem for elem in date_file_list if elem==date_file_list[1]]
-
-    file_name = []
-  #  file_date = []
-
-    #print "%-40s %s" % ("filename:", "last modified:")
-    for file in selected_pms:
+    #required_ts = timestamps[0]
+    selected_names = [elem[1] for elem in date_file_list if elem[0]==required_ts]
+    selected_timestamps = [elem[1] for elem in date_file_list if elem[0]==required_ts]
+    #file_names = []
+    #for n in selected_pms:
         # extract just the filename
-        file_name.append(file[1])    
-        # convert date tuple to DD/MM/YYYY HH:MM:SS format
-  #      file_date_temp =(time.strftime("%d/%m/%y %H:%M:%S", file[0]))
-  #      file_date.append(file_date_temp)
+        #file_names.append(n)    
     
-    return file_name
-
-#print "%-40s %s" % (file_name, file_date)
+    #print file_names[1]
+    return selected_names,selected_timestamps
 
 # save task
 if __name__ == "__main__":
-    list_of_files =  file_list('/home/ops/rf/RF-libera/RF_Postmortems/')
-    print list_of_files[:len(list_of_files)-4]
+    list_of_files, list_of_timestamps =  file_list('/home/ops/rf/RF-libera/RF_Postmortems/')
+    #print list_of_files
 
--- rf_pm_frontend.py
+++ rf_pm_frontend.py
@@ -15,24 +15,33 @@ from qt import *
 from scipy.io import loadmat
 import sys
 from datetime import datetime
+from rf_pm_file_select import *
 
-print 'args:', sys.argv
-filenames = []
+path_to_files = '/home/ops/rf/RF-libera/RF_Postmortems/'
 
+# if given an argument (whatever) will use file_list to find out the required filenames otherwise it will ask for a file through the browser.
 if len(sys.argv) > 1:
-    for h in range(1,len(sys.argv)):
-        filenames.append(sys.argv[h])
+    filenames, timestamps = file_list(path_to_files)
+    #for h in range(1,len(sys.argv)):
+        #name_trimmed = sys.argv[h]
+        #print name_trimmed
+        #name_trimmed = name_trimmed[:-6]
+        #print name_trimmed
+        #filenames.append(name_trimmed)
 else:
+    filenames = []
+    #timestamps = []
     # No filename specified, so prompt for filename!
     import tkFileDialog
     filename = tkFileDialog.askopenfilename(
-        initialdir = '/home/ops/rf/RF-libera/RF_Postmortems',
+        initialdir = path_to_files,
         filetypes = ['mat *.mat'])
     if not filename:
         sys.exit(1)
     else:
-        filenames.append(filename)
-
+        filenames.append(filename[1])
+        #timestamps.append(filename[0])
+        
 utctime = []
 pmN = []
 cavity = []
@@ -45,7 +54,7 @@ for filename in filenames:
     # Channel 0 is RF reference so all other channels are relative to this.
     pm = pm / pm[0]
     pmN.append(pm)
-
+    
 # Plotting phase and magnitude plots
 
 # generating the title display
@@ -61,16 +70,15 @@ for d in range(len(utctime)):
     if cavity[d]!=cavity[0]:
         same_cavity = 'False'
 
-
 # case for one dataset
 if len(utctime) == 1:
-    time = datetime.fromtimestamp(utctime[0])   
-    disptime = ['RF postmortem for ',str(time.day),'/',str(time.month),'/',str  (time.year),' at ',str(time.hour),':',str(time.minute),'.',str(time.second),' (cavity ',cavity[0],')']
+    title_time = datetime.fromtimestamp(utctime[0])   
+    disptime = ['RF postmortem for ',str(title_time.day),'/',str(title_time.month),'/',str  (title_time.year),' at ',str(title_time.hour),':',str(title_time.minute),'.',str(title_time.second),' (cavity ',cavity[0],')']
     disp = ''.join(disptime)
 # for multiple data sets with the same timestamp to the nearest second (i.e. the same incedent for all cavities)
 elif len(utctime) > 1 and same_ts=='True':
-    time = datetime.fromtimestamp(utctime[0])   
-    disptime = ['RF postmortem for ',str(time.day),'/',str(time.month),'/',str  (time.year),' at ',str(time.hour),':',str(time.minute),'.',str(time.second)]
+    title_time = datetime.fromtimestamp(utctime[0])  
+    disptime = ['RF postmortem for ',str(title_time.day),'/',str(title_time.month),'/',str  (title_time.year),' at ',str(title_time.hour),':',str(title_time.minute),'.',str(title_time.second)]
     disptime = ''.join(disptime)
     disp = disptime + '\nBlue is cavity ' + str(cavity[0]) + ' Green is cavity ' + str(cavity[1])
 elif len(utctime) > 1 and same_cavity=='True':
