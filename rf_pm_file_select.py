72d9b03055fce2b1b93612493d890a97587928be
mga83 <mga83@e099a375-04f9-0310-9d5f-a741eaff62e1>
2008-10-08 12:56:27 +0000

    RF postmortem: Layout changes only.  Fix bogus DOS line endings, ensure all lines no longer than 80 characters, put spaces after commas.
    git-svn-id: svn+ssh://serv0002.cs.diamond.ac.uk/home/subversion/repos/controls/diamond/trunk/ioc/CS/CS-DI-IOC-07@24206 e099a375-04f9-0310-9d5f-a741eaff62e1

--- rf_pm_file_select.py
+++ rf_pm_file_select.py
@@ -1,12 +1,12 @@
 #!/dls_sw/tools/bin/python2.4
 
-# retrieve the file information from a selected folder
-# sort the files by last modified date/time and display in order newest file first
+# retrieve the file information from a selected folder, sort the files by last
+# modified date/time and display in order newest file first
 import os
 import glob
-import time,sys,re
+import time, sys, re
 
-def file_list(out_length,root,cavity,pm_day,pm_month,pm_year,pm_time):
+def file_list(out_length, root, cavity, pm_day, pm_month, pm_year, pm_time):
     #root is the path to the files.
     #head of pm filename
     pm_head = 'rf_postmortem-0'
@@ -23,16 +23,19 @@ def file_list(out_length,root,cavity,pm_day,pm_month,pm_year,pm_time):
     if pm_time == '*':
         pm_time = '([0-9][0-9]:[0-9][0-9]:[0-9][0-9])'
    #constructing search value
-    search_string= str(root) + pm_head + str(cavity) + '-' + str(pm_year) + '-' + str(pm_month) + '-' + str(pm_day) + 'T' + str(pm_time) + '.mat'
+    search_string = \
+        str(root) + pm_head + str(cavity) + '-' + \
+        str(pm_year) + '-' + str(pm_month) + '-' + str(pm_day) + \
+        'T' + str(pm_time) + '.mat'
     val = re.compile(search_string)
     for folder in glob.glob(root):
-#        print "folder =", folder
-     # select the type of file, for instance *.mat or all files *.*
+#        print 'folder =', folder
+        # select the type of file, for instance *.mat or all files *.*
         for file in glob.glob(folder + '/' +pm_head +'*.mat'):
             if val.match(file):
                 # get date from filename
                 pm_date = file[55:-4]
-                pm_date = time.strptime(pm_date,"%Y-%m-%dT%H:%M:%S")
+                pm_date = time.strptime(pm_date, '%Y-%m-%dT%H:%M:%S')
                 # create list of tuples ready for sorting by date
                 date_file_tuple = (pm_date, file)
                 date_file_list.append(date_file_tuple)
@@ -41,14 +44,15 @@ def file_list(out_length,root,cavity,pm_day,pm_month,pm_year,pm_time):
 
     date_file_list.sort()
     date_file_list.reverse()  # newest mod date now first
-    # Changing from 1 list of 2D tupples to 2 tupples the length of the original list
+    # Changing from 1 list of 2D tuples to 2 tuples the length of the original
+    # list
     names = zip(*date_file_list)[1]
     file_list = names[0:int(out_length)]
     return file_list
 
 
 # save task
-if __name__ == "__main__":
+if __name__ == '__main__':
     #list_of_files, list_of_timestamps =  file_list(*sys.argv[1:])
     #print list_of_files
     #print list_of_timestamps
--- rf_pm_frontend.py
+++ rf_pm_frontend.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python2.4
 
-# This is the frontend for the RF postmortem. It displays magnitude and phase infromation for channels 2-4. It uses channel 1 as the reference signal (i.e. the RF)
+# This is the frontend for the RF postmortem. It displays magnitude and phase
+# infromation for channels 2-4. It uses channel 1 as the reference signal
+# (i.e. the RF)
 
 from pkg_resources import require
 require('cothread==1.5')
@@ -15,18 +17,21 @@ from qt import *
 from scipy.io import loadmat
 import sys
 from datetime import datetime
-from rf_pm_file_select import *
+from rf_pm_file_select import file_list
 
 path_to_files = '/dls/ops-data/Postmortems/RF_Postmortems/'
 
-# if given an argument (whatever) will use file_list to find out the required filenames otherwise it will ask for a file through the browser.
+# if given an argument (whatever) will use file_list to find out the required
+# filenames otherwise it will ask for a file through the browser.
 print len(sys.argv)
 if len(sys.argv) == 7:
-    filenames = file_list(sys.argv[1],path_to_files,sys.argv[2],sys.argv[3],sys.argv[4],sys.argv[5],sys.argv[6])
+    filenames = file_list(
+        sys.argv[1], path_to_files, sys.argv[2], sys.argv[3],
+        sys.argv[4], sys.argv[5], sys.argv[6])
     print filenames
 elif len(sys.argv) == 2:
     # takes the n newest files
-    filenames = file_list(sys.argv[1],path_to_files,'*','*','*','*','*')
+    filenames = file_list(sys.argv[1], path_to_files, '*', '*', '*', '*', '*')
     print filenames
 else:
     filenames = []
@@ -61,8 +66,8 @@ utctime.reverse()
 # Plotting phase and magnitude plots
 
 # generating the title display
-a = axes([0,0,1,0.93],axisbg='grey')
-setp(a,xticks=[],yticks=[])
+a = axes([0, 0, 1, 0.93], axisbg='grey')
+setp(a, xticks=[], yticks=[])
 
 same_ts = 'True'
 same_cavity = 'True'
@@ -74,21 +79,30 @@ for d in range(len(utctime)):
         same_cavity = 'False'
 
 title_time = datetime.fromtimestamp(utctime[0])  
-disptime = ['RF postmortem for ',str(title_time.day),'/',str(title_time.month),'/',str  (title_time.year),' at ',str(title_time.hour),':',str(title_time.minute)]
+disptime = [
+    'RF postmortem for ', str(title_time.day), '/', str(title_time.month),
+    '/', str(title_time.year), ' at ', str(title_time.hour), ':',
+    str(title_time.minute)]
 disptime = ''.join(disptime)
 
 # case for one dataset
 if len(utctime) == 1:
     disp = disptime + ' (cavity ' + str(cavity[0]) + ')'
-# for multiple data sets with the same timestamp to the nearest second (i.e. the same incident for all cavities)
+# for multiple data sets with the same timestamp to the nearest second (i.e.
+# the same incident for all cavities)
 elif len(utctime) > 1 and same_ts=='True':
-    disp = disptime + '\nBlue is cavity ' + str(cavity[0]) + ' Green is cavity ' + str(cavity[1])
+    disp = disptime + '\nBlue is cavity ' + str(cavity[0]) + \
+        ' Green is cavity ' + str(cavity[1])
 elif len(utctime) > 1 and same_cavity=='True':
-    colour_list = ['blue','green','red','cyan','magenta','yellow','black','white']
+    colour_list = [
+        'blue', 'green', 'red', 'cyan', 'magenta', 'yellow', 'black', 'white']
     disp_temp = ['RF postmortems for cavity ', str(cavity[0]), '\n']
     for f in range(len(utctime)):
         date_temp = datetime.fromtimestamp(utctime[f])
-        disp_temp.extend([colour_list[f], ' is ',str(date_temp.day),'/',str(date_temp.month),'/',str(date_temp.year),' at ',str(date_temp.hour),':',str(date_temp.minute),'   '])
+        disp_temp.extend([
+            colour_list[f], ' is ', str(date_temp.day), '/',
+            str(date_temp.month), '/', str(date_temp.year), ' at ',
+            str(date_temp.hour), ':', str(date_temp.minute), '   '])
     disp = ''.join(disp_temp)
     # need to add legend somewhere
 else:
@@ -96,26 +110,26 @@ else:
 
 title(disp)    
 ioff()
-head1 = ['Cavity Phase', 'Forward Phase','Reflected Phase']
-head2 = ['Cavity Magnitude', 'Forward Magnitude','Reflected Magnitude']
-x_axis = linspace(-1000,1000,2000)
-for n in range(1,4):
-    axes([0.1,0.08+(n-1)*0.3,0.35,0.2])
+head1 = ['Cavity Phase', 'Forward Phase', 'Reflected Phase']
+head2 = ['Cavity Magnitude', 'Forward Magnitude', 'Reflected Magnitude']
+x_axis = linspace(-1000, 1000, 2000)
+for n in range(1, 4):
+    axes([0.1, 0.08+(n-1)*0.3, 0.35, 0.2])
     title(head1[n-1])
     xlabel('Turns')
     ylabel('Degrees')
     for h in range(len(utctime)):
-        plot(x_axis,180/pi * unwrap(angle(pmN[h].T[-2000:, n])))
+        plot(x_axis, 180/pi * unwrap(angle(pmN[h].T[-2000:, n])))
         grid(color='grey', linestyle=':', linewidth=1, alpha=0.5)
         hold
     hold
     axvline(0)  
-    axes([0.6,0.08+(n-1)*0.3,0.35,0.2])
+    axes([0.6, 0.08+(n-1)*0.3, 0.35, 0.2])
     title(head2[n-1])
     xlabel('Turns')
     ylabel('Signal (a.u.)')
     for h in range(len(utctime)):
-        plot(x_axis,abs(pmN[h].T[-2000:, n]))
+        plot(x_axis, abs(pmN[h].T[-2000:, n]))
         grid(color='grey', linestyle=':', linewidth=1, alpha=0.5)
         hold
     hold
